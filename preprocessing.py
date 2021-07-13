3d409c0a0c546d27f44ce006639a14390dfa64ed
Venugopal Bhatia <venugopalbhatia@gmail.com>
2021-07-13 15:27:48 +0530

    add methods to extract hashtags and mentions and lemmatize tweets

--- preprocessing.py
+++ preprocessing.py
@@ -6,17 +6,34 @@ from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet
+import re
 
 class Preprocessor:
 
-    def __init__(self,lemmatizer):
+    def __init__(self):
         self.lemmatizer = WordNetLemmatizer()
         self.stopwords = stopwords.words('english')
         self.stopwords.extend(list(string.punctuation))
         self.pos_tag = nltk.pos_tag
 
+######### Methods to apply to dataframes ###############################################
+    
     def simple_clean(self,df,msgColumn,cleanColumn):
-        df[cleanColumn] = df[msgColumn].replace("^RT.*:|\s{2,}|…"," ",regex=True).str.strip()
+        df['retweeted_from'] = df[msgColumn].apply(lambda x: re.findall("^RT.*:",x)) #Extract retweet handle
+        df['mentions'] = df[msgColumn].apply(lambda x: re.findall("@([a-zA-Z0-9_]{1,50})",x)) #Extract mentions
+        df['hashtags'] = df[msgColumn].apply(lambda x: re.findall("#([a-zA-Z0-9_]{1,50})",x)) #Extract hashtags
+        df['links'] = df[msgColumn].apply(lambda x: re.findall("https[a-zA-Z0-9_//:.]{1,100}",x)) #Extract links
+        df[cleanColumn] = df[msgColumn].replace("^RT.*:|\s{2,}|…|&amp;|@([a-zA-Z0-9_]{1,50})|https[a-zA-Z0-9_//:.]{1,100}"," ",regex=True).str.strip() #Dont remove hashtags as they may provide context
+
+
+    def clean_tweets(self,df,msgColumn,wordTokens):
+        df[wordTokens] = df[msgColumn].apply(self.gen_word_tokens) # not converting to lowercase since it may lead to loss of information
+        
+    
+    def lemmatize_tweets(self,df,wordTokens,wordTokensLemmatized):
+        df[wordTokensLemmatized] = df[wordTokens].apply(self.lemmatize_message)
+
+######### Methods to apply to individual messages ######################################
 
     def gen_word_tokens(self,message):
         words = word_tokenize(message)
@@ -25,19 +42,12 @@ class Preprocessor:
             if w.lower() not in self.stopwords:
                 wordList.append(w)
         return wordList
-
-    def clean_tweets(self,df,msgColumn,wordTokens):
-        df[wordTokens] = df[msgColumn].apply(self.gen_word_tokens)
-        
-    
-    def lemmatize_tweets(self,df,wordTokens,wordTokensLemmatized):
-        df[wordTokensLemmatized] = df[wordTokens].apply(self.lemmatize_message)
         
     def lemmatize_message(self,wordTokens):
         posTags = self.pos_tag(wordTokens)
         lemmatized_words = []
         for w in posTags:
-            lemmatized_words.append(self.lemmatizer.lemmatize(w[0],pos=get_simple_tag(w[1])))
+            lemmatized_words.append(self.lemmatizer.lemmatize(w[0],pos=self.get_simple_tag(w[1])))
         
         return lemmatized_words
     
