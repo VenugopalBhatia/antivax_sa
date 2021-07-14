a18a4770076b4bd0f0a833d6857f2d28daa6b852
Venugopal Bhatia <venugopalbhatia@gmail.com>
2021-07-14 19:56:49 +0530

    resolve minor bugs

--- models/afinn_sa.py
+++ models/afinn_sa.py
@@ -2,6 +2,6 @@ from afinn import Afinn
 import pandas as pd
 
 
-def sentiment_score(df,message_cleaned):
+def sentiment_scores(df,message_cleaned):
     afn = Afinn()
     df['afinn_score'] = df[message_cleaned].apply(afn.score)
