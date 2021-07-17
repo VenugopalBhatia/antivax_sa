6a5702816b319871a1e031293b01fdaea84dea95
Venugopal Bhatia <venugopalbhatia@gmail.com>
2021-07-17 12:57:39 +0530

    gen dictionaries for models and their respective feature space

--- models/hyperopt_pt.py
+++ models/hyperopt_pt.py
@@ -1,10 +1,11 @@
-from hyperopt import fmin, tpe,hp, STATUS_OK, Trials
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
 from sklearn.model_selection import cross_val_score
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from models.models_hyperparams import get_model,get_fspace
 from .. import utils
 
 ####### Methods for minimization and logging each time step taken by hyperopt ####
@@ -12,23 +13,16 @@ from .. import utils
 
 class optimize_model:
 
-    def __init__(self,fspace,algo,max_evals,df,message_tokenized,x,y):
+    def __init__(self,model,algo,max_evals,df,message_tokenized,x,y):
         self.trials = Trials()
-        self.fspace = fspace
+        self.fspace = get_fspace(model)
         self.algo = algo
         self.max_evals = max_evals
         self.df = df
         self.message_tokenized = message_tokenized
         self.x = x
         self.y = y
-        self.model_dict = {
-            'svm': SVC(**params),
-            'naive_bayes': MultinomialNB(**params),
-            'random_forest': RandomForestClassifier(**params),
-            'decision_tree': DecisionTreeClassifier(**params),
-            'knn': KNeighborsClassifier(**params)
-
-       }
+        self.model = get_model(model)
     
 
     # Method to return stratified cross validation scores 
@@ -43,8 +37,8 @@ class optimize_model:
         
         Y = self.df[self.y].values
 
-        clf = self.model_dict['model']
-        del params['model']
+        clf = self.model
+        
 
         return cross_val_score(clf,X,Y).mean()  #Default is StratifiedKFold for sklearn based classifiers
        
@@ -63,7 +57,7 @@ class optimize_model:
 
         best = fmin(f,space = self.fspace,algo = self.algo, max_evals = self.max_evals,trials = self.trials)
 
-        return best
+        return best,self.trials
     
 
    
